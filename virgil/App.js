629c8c170f1baeca8a171e496ef0ae5f3bf39bd4
tanzeelak <tnkhan8042@gmail.com>
2018-03-03 11:49:32 -0800

    init nav

--- virgil/App.js
+++ virgil/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import {Button, NavigatorIOS, Text, View} from 'react-native';
 
-export default class App extends React.Component {
+export default class NavigatorIOSApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <NavigatorIOS
+        initialRoute={{
+          component: MyScene,
+          title: 'My Initial Scene',
+          passProps: {index: 1},
+        }}
+        style={{flex: 1}}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class MyScene extends React.Component {
+  static propTypes = {
+    route: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+    navigator: PropTypes.object.isRequired,
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this._onForward = this._onForward.bind(this);
+  }
+
+  _onForward() {
+    let nextIndex = ++this.props.index;
+    this.props.navigator.push({
+      component: MyScene,
+      title: 'Scene ' + nextIndex,
+      passProps: {index: nextIndex},
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: {this.props.title}</Text>
+        <Button
+          onPress={this._onForward}
+          title="Tap me to load the next scene"
+        />
+      </View>
+    );
+  }
+}
