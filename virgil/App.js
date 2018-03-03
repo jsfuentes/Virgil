ccc4b54dee8cb2d2dd9f7d0326a3abf4d91ee4fe
tanzeelak <tnkhan8042@gmail.com>
2018-03-03 13:13:54 -0800

    basically nothing

--- virgil/App.js
+++ virgil/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 import { StackNavigator } from 'react-navigation';
-import HomeScreen from './HomeScreen';
-import OptionScreen from './OptionScreen';
-import CameraScreen from './CameraScreen';
+import HomeScreen from './screens/HomeScreen';
+import OptionScreen from './screens/OptionScreen';
+import CameraScreen from './screens/CameraScreen';
 
 const Navigation = StackNavigator({
     Home: {screen: HomeScreen},
