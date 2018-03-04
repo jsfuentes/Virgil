55ba25ae60157479d46f9c273e67b60d663e2c46
Jorge Fuentes <jsjfuentesj@yahoo.com>
2018-03-03 20:34:24 -0800

    Server returns audio after api call

--- python/application.py
+++ python/application.py
@@ -10,25 +10,36 @@ import vision
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-JVM_TOKEN = getNewJVMToken()
+JVM_TOKEN = speech.getNewJVMToken()
 JVM_CREATION_TIME = time()
 
 @app.route('/')
 def index():
-    return "<h1> I LOVE YOU </h1>"
+    print(JVM_CREATION_TIME)
+    return "<marquee><h1 style='font-size:300px;'> I LOVE YOU </h1></marquee>"
 
 #TODO: Take image in format
 @app.route('/api/v1/image', methods=['POST'])
 def image():
+    global JVM_CREATION_TIME
+    global JVM_TOKEN
     # GET THE IMAGE FROM THE ARGUMENTS IN THE FORMAT
     img = "http://onpointfresh.com/wp-content/uploads/2016/03/95559ca9a79f7da23522cb702e5eb2e8.jpg"
+    #JVM lasts for 10 minutes, so make sure its less than 8 minutes old
+    if (time() - JVM_CREATION_TIME) > (8 * 60):
+        JVM_TOKEN = speech.getNewJVMToken()
+        JVM_CREATION_TIME = time()
 
-
-    return "HELLO"
+    subject = vision.getSubject(img)
+    audio = speech.getAudioForText(subject, JVM_TOKEN)
+    f = open("test_input.mp3", 'wb')
+    f.write(audio)
+    f.close()
+    return audio
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    app.run()
+    app.run(port=4000)
