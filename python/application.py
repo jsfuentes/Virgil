3cafc639c2b90fd9f8db388d303211213e4cf550
Jorge Fuentes <jsjfuentesj@yahoo.com>
2018-03-04 04:09:55 -0800

    Add audio api

--- python/application.py
+++ python/application.py
@@ -10,6 +10,7 @@ import vision
 import screenshot
 
 app = Flask(__name__)
+manager = Manager(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 JVM_TOKEN = speech.getNewJVMToken()
@@ -19,6 +20,21 @@ JVM_CREATION_TIME = time()
 def index():
     print(JVM_CREATION_TIME)
     return "<marquee><h1 style='font-size:300px;'> I LOVE YOU </h1></marquee>"
+
+@app.route('/audio')
+def audio():
+    global JVM_CREATION_TIME
+    global JVM_TOKEN
+    # GET THE IMAGE FROM THE ARGUMENTS IN THE FORMAT
+    img = "http://onpointfresh.com/wp-content/uploads/2016/03/95559ca9a79f7da23522cb702e5eb2e8.jpg"
+    #JVM lasts for 10 minutes, so make sure its less than 8 minutes old
+    if (time() - JVM_CREATION_TIME) > (8 * 60):
+        JVM_TOKEN = speech.getNewJVMToken()
+        JVM_CREATION_TIME = time()
+
+    subject = vision.getSubject(img)
+    audio = speech.getAudioForText(subject, JVM_TOKEN)
+    return audio
 
 #TODO: Take image in format
 @app.route('/api/v1/image', methods=['GET', 'POST'])
@@ -55,7 +71,4 @@ def image():
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    app.debug = True
-    app.run(port=2000)
+    manager.run()
